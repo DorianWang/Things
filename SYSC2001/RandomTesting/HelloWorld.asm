68c4b5a0edb924ca62d1b18bc97bb195bfe8a28a
dustan1 <goadustan@gmail.com>
2016-09-20 13:13:59 -0400

    asdfef

--- SYSC2001/RandomTesting/HelloWorld.asm
+++ SYSC2001/RandomTesting/HelloWorld.asm
@@ -2,17 +2,60 @@
 ; This should be a comment. 
 ; The below code is (hopefully) a hello world program.
 	
-	
+;;{
 testfunc:
 	mov		CX, 00FFh
 	mov 	BX, 0F0Fh
 	ret
+;;}
+
+
+;Adds another space to the list.
+increaseTestVarSize:
+;;{
+	push CX
 	
+	mov CX, [1000h]
+	add CX, 2h
+	mov [1000h], CX
 	
+	pop CX
 	
+	ret
+;;}
+
+;Clobbers whatever is in the last spot of the list, and reduces the size. Also refills the former last spot with FFFF.
+decreaseTestVarSize:
+;;{
+	cmp		[1000h], 1002h	; If 1000h is less than 1002h (IE, list is empty, skip doing things.)
+	jle 	skipDecreaseTestVarValue
 	
+	push CX
 	
+	mov CX, [1000h]
+	sub CX, 2
+	mov [1000h], CX
 	
+	pop CX
+	
+	skipDecreaseTestVarValue:
+	
+	ret
+;;}
+
+
+;Adds a value to the stack, and increases its size.
+;Uses the value contained in the SI register, no registers should be affected.
+pushTestVar:
+;;{
+	mov CX, [1000h]
+	mov [CX], SI
+	call increaseTestVarSize
+	
+	ret
+;;}
+
+
 start:
 
         mov     DX, 04E9h	; load the display port I/O address into register DX (16 bits wide)
@@ -21,6 +64,13 @@ start:
 		
         ;mov     AL, 'i'		; load the ASCII value of the character 'H' into register AL (8 bits wide)
         ;out     DX, al		; send the ASCII character in AL out to the display at the port number in DX
+		
+		mov CX, 1002h
+		mov [1000h], CX
+		mov SI, 0123h
+		call pushTestVar
+		mov SI, 4251h
+		call pushTestVar
 		
 		
 		mov 	CX, "an"
