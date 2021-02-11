dd17c8a4094b4783de2224d9e289b55163ede5ed
dwaw <goadustan@gmail.com>
2021-02-10 22:00:28 -0500

    Working Version
    This version works, somehow.

--- SYSC4XXX/SYSC4001/Assignment1/MMULT.c
+++ SYSC4XXX/SYSC4001/Assignment1/MMULT.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
+#include <time.h>
+#include <semaphore.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -10,35 +15,20 @@
 #include "row_calc.h"
 #include "test_case_constants.h"
 
-
 #define SIG_CONTINUE 12
+#define MICRO_SEC_IN_SEC 1000000
 
-
-void sig_handler(int sig)
-{
-   if (sig == SIG_CONTINUE){
-      return;
-   }
-}
 
 
 int main()
 {
-
-   struct sigaction signaller;
-   signaller.sa_handler = sig_handler;
-   sigemptyset(&signaller.sa_mask);
-   signaller.sa_flags = 0;
-    To prevent children from racing ahead of parent, could be used to recycle threads if required.
-   sigaction(SIG_CONTINUE, &signaller, 0);
-
    struct timeval start, end; // Timing variables, to time each test case.
    pid_t children[4]; // Hold children, to poke them awake
    int fd[2]; // Used to transfer largest number data through pipe.
 
-   // This should create a memory space which can hold all 3 matrices.
+   // This should create a memory space which can hold all 3 matrices, and a semaphore.
    int shmid;
-   shmid = shmget(IPC_PRIVATE, (sizeof(m_4x4_t) * 3),
+   shmid = shmget(IPC_PRIVATE, (sizeof(m_4x4_t) * 3) + sizeof(sem_t),
                   0666 | IPC_CREAT);
    if (shmid == -1){
       fprintf(stderr, "shmget failed\n");
@@ -52,54 +42,75 @@ int main()
       exit(EXIT_FAILURE);
    }
 
+   sem_t* matrices_ready_semaphore = (sem_t*)(matrix_array + 4); // Pointer arithmetic, for when you hate the future.
+   sem_init(matrices_ready_semaphore, 1, 0);
 
-   gettimeofday(&start, NULL);
-   pipe(fd);
-   int i; // I'm not actually sure this is needed for modern C, but some old versions need it.
-   for (i = 0; i < 4; i++){
-      pid_t pid_val = fork();
-      switch(pid_val)
-      {
-      case -1:
-         perror("fork failed");
-         exit(1);
-      case 0: // I am the child
-         pause();
-         setvbuf(stdout, NULL, _IONBF, 0); // Print to terminal immediately, instead of buffering.
-         close(fd[0]); // Children close the reader end of the pipe.
-         MMultStruct temp = {(matrix_array), (matrix_array + 1), (matrix_array + 2)};
-         int return_val = calc_row_mmult(i, temp);
-         write(fd[1], &return_val, sizeof(int)); // Should be thread safe/atomic?
-         close(fd[1]); // Close the given pipe when you're done?
-         break;
-      default: // Parent
-         children[i] = pid_val;
+
+   MMultStruct* tests_array;
+   int total_test_cases = assemble_test_cases(&tests_array);
+   int test_num;
+   for(test_num = 0; test_num < total_test_cases; test_num++){
+
+      gettimeofday(&start, NULL);
+      pipe(fd);
+      int i; int j;// I'm not actually sure this is needed for modern C, but some old versions need it.
+      for (i = 0; i < 4; i++){
+         pid_t pid_val = fork();
+         switch(pid_val)
+         {
+         case -1:
+            perror("fork failed");
+            exit(1);
+         case 0: // I am the child
+            sem_wait(matrices_ready_semaphore);
+            printf("Working with row %d!\n", i);
+            close(fd[0]); // Children close the reader end of the pipe.
+            MMultStruct temp = {(matrix_array), (matrix_array + 1), (matrix_array + 2)};
+            int return_val = calc_row_mmult(i, temp);
+            write(fd[1], &return_val, sizeof(int)); // Should be thread safe/atomic?
+            close(fd[1]); // Close the given pipe when you're done?
+            exit(EXIT_SUCCESS);
+         default: // Parent
+            children[i] = pid_val;
+         }
       }
-   }
-   close(fd[1]); // The parent closes the writer end of their pipe.
+      close(fd[1]); // The parent closes the writer end of their pipe.
 
-   //Tell children to run.
-   for (i = 0; i < 4; i++){
-      kill(children[i], SIG_CONTINUE);
-   }
+      memcpy((matrix_array), *(tests_array[test_num].first), sizeof(m_4x4_t));
+      memcpy((matrix_array + 1), *(tests_array[test_num].second), sizeof(m_4x4_t));
 
-   //Wait for all children to return, and then try to read the number from the pipe.
-   //If the number is bigger than the current highest number, then use it instead.
-   int max_val = INT_MIN;
-   for (i = 0; i < 4; i++){
-      waitpid(children[i], NULL);
-      int row_max;
-      read(fd[0], &row_max, sizeof(int));
-      if (row_max > max_val){
-         max_val = row_max;
+      //Tell children to run. If semaphores aren't used it is possible for children to rush ahead before.
+      for (i = 0; i < 4; i++){
+         sem_post(matrices_ready_semaphore);
       }
+
+      //Wait for all children to return, and then try to read the number from the pipe.
+      //If the number is bigger than the current highest number, then use it instead.
+      int max_val = INT_MIN;
+      for (i = 0; i < 4; i++){
+         waitpid(children[i], NULL, 0);
+         int row_max;
+         read(fd[0], &row_max, sizeof(int));
+         //printf("Value found was %d\n", row_max);
+         if (row_max > max_val){
+            max_val = row_max;
+         }
+      }
+      close(fd[0]); // Close the pipe, just in case.
+      gettimeofday(&end, NULL);
+      printf("The time taken (for the forks and computation): %dus\n", (end.tv_sec - start.tv_sec) * MICRO_SEC_IN_SEC + (end.tv_usec - start.tv_usec));
+      printf("The two input matrices were:\n");
+      print_m_4x4_t(matrix_array + 0);
+      printf("------------------------------------make\n");
+      print_m_4x4_t(matrix_array + 1);
+      printf("The output matrix is:\n");
+      print_m_4x4_t(matrix_array + 2);
+      printf("The maximum value in the output is %d!\n\n", max_val);
+
    }
-   close(fd[0]); // Close the pipe, just in case.
-   printf("The maximum value in the output is %d!", max_val);
 
-   printf("Program complete!\n");
-
-   return 0;
+   printf("Program is complete!\n");
+   return EXIT_SUCCESS;
 }
 
 
--- SYSC4XXX/SYSC4001/Assignment1/row_calc.h
+++ SYSC4XXX/SYSC4001/Assignment1/row_calc.h
@@ -2,9 +2,14 @@
 #define ROW_CALC_H_INCLUDED
 
 #include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//https://stackoverflow.com/questions/14808908/pointer-to-2d-arrays-in-c
-typedef int m_4x4_t[4][4];
+#define MATRIX_SIZE_CONST 4
+
+// https://stackoverflow.com/questions/14808908/pointer-to-2d-arrays-in-c
+// First value is row, second is column
+typedef int m_4x4_t[MATRIX_SIZE_CONST][MATRIX_SIZE_CONST];
 
 // The program requirements state to multiply 4x4 matrices, so this program only supports 4x4.
 typedef struct matrix_multiplication_4_by_4
@@ -19,7 +24,7 @@ typedef struct matrix_multiplication_4_by_4
 
 
 int calc_row_mmult(int row_value, MMultStruct input_matrices);
-
+void print_m_4x4_t(m_4x4_t* input);
 
 
 
