89d08e2e53e42f099ce063ec0410459115296a6a
dustan1 <goadustan@gmail.com>
2021-03-05 17:59:06 -0500

    Created A2 for SYSC4001

---
+++ SYSC4XXX/SYSC4001/Assignment2/sem_func.h
@@ -0,0 +1,22 @@
+#ifndef SEM_FUNC_H_INCLUDED
+#define SEM_FUNC_H_INCLUDED
+
+#include <stdio.h>
+
+#include <errno.h>
+#include <sys/sem.h>
+
+union semun {
+   int val;
+   struct semid_ds *buf;
+   unsigned short *array;
+};
+
+
+int set_semvalue(int sem_id, int val, int sem_index);
+void del_semvalue(int sem_id);
+int semaphore_p(int sem_id, int sem_index);
+int semaphore_v(int sem_id, int sem_index);
+
+#endif // SEM_FUNC_H_INCLUDED
+
