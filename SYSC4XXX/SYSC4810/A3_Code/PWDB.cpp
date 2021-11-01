3f949e4513e894057a75ff1f8f04d5b3104b8133
dustan1 <goadustan@gmail.com>
2021-11-01 14:29:26 -0400

    Almost there...

--- SYSC4XXX/SYSC4810/A3_Code/PWDB.cpp
+++ SYSC4XXX/SYSC4810/A3_Code/PWDB.cpp
@@ -16,10 +16,12 @@ PermissionsDB::PermissionsDB(const std::filesystem::path& newDBPath)
    passwordFilter.read_password_rules_file(rules);
    passwordFilter.read_common_patterns_file(commonPasses);
    passwordFilter.read_restricted_passwords_file(badPasses);
+   passwordFilter.print_values();
    groupList.insert(std::pair<uint_fast64_t, GroupType>(0, GroupType{std::string("DEFAULT"), 0}));
-   userList.insert(std::pair<uint_fast64_t, UserEntry>(0, UserEntry(0, 0)));
+   userList.insert(std::pair<uint_fast64_t, UserEntry>(0, UserEntry(0, 0, std::string("DEFAULT"), std::string("DEFAULT"))));
    nextUID = 1; nextGID = 1;
-
+   assert(sizeof(uint64_t) == (sizeof(unsigned char) * 8)); // My code will explode if this is false.
+   isAfterHours = false;
 }
 
 PermissionsDB::~PermissionsDB()
@@ -34,25 +36,14 @@ void PermissionsDB::set_DB_path(const std::filesystem::path& newDBPath)
    DBPath = newDBPath;
 }
 
-bool PermissionsDB::scrypt_hashing(std::string& password, uint64_t* saltOut, unsigned char* hashOut, size_t outSize)
+bool PermissionsDB::scrypt_hashing(const std::string& password, uint64_t salt, unsigned char* hashOut, size_t outSize)
 {
    if (EVP_PKEY_CTX_set1_pbe_pass(pctx, password.c_str(), password.length()) <= 0) {
       std::cerr << "Failed to set password\n";
       return false;
    }
-
    unsigned char buffer[8];
-   if (RAND_bytes(buffer, 8) == 1){
-      *saltOut = 0;
-      for (size_t i = 0; i < 8; i++){
-         *saltOut += ((uint64_t) buffer) << (i * 8);
-      }
-   }
-   else{
-      std::cerr << "Failed to get random salt\n";
-      return false;
-   }
-
+   memcpy(buffer, &salt, 8);
    if (EVP_PKEY_CTX_set1_scrypt_salt(pctx, buffer, 8) <= 0) {
       std::cerr << "Failed to set salt\n";
       return false;
@@ -77,6 +68,38 @@ bool PermissionsDB::scrypt_hashing(std::string& password, uint64_t* saltOut, uns
    return true;
 }
 
+bool PermissionsDB::scrypt_generate_hash(const std::string& password, UserEntry& usr)
+{
+   unsigned char buffer[8];
+   if (RAND_bytes(buffer, 8) == 1){
+      memcpy(&(usr.salt), buffer, 8);
+   }
+   else{
+      std::cerr << "Failed to get random salt\n";
+      return false;
+   }
+   return scrypt_hashing(password, usr.salt, usr.hashedPass, HASH_OUTPUT_LENGTH);
+}
+
+bool PermissionsDB::scrypt_hash_check(const std::string& password, const UserEntry& usr)
+{
+   unsigned char givenHash[HASH_OUTPUT_LENGTH];
+   if (scrypt_hashing(password, usr.salt, givenHash, HASH_OUTPUT_LENGTH)){
+      return memcmp(givenHash, usr.hashedPass, HASH_OUTPUT_LENGTH) == 0;
+   }
+   return false; // Hashing failed.
+}
+
+uint_fast64_t PermissionsDB::get_UID_from_username(const std::string& username)
+{
+   // Should have made a bidirectional map >.<
+   for (auto iter : userList){
+      if (iter.second.username == username){
+         return iter.first;
+      }
+   }
+   return 0;
+}
 
 uint_fast64_t PermissionsDB::add_user(std::string name, std::string username, uint_fast64_t GID, std::string password)
 {
@@ -85,6 +108,9 @@ uint_fast64_t PermissionsDB::add_user(std::string name, std::string username, ui
    }
    if (passwordFilter.check_password(username, password) != 0){
       return 0; // Bad password.
+   }
+   if (get_UID_from_username(username)){
+      return 0; // User already exists.
    }
    size_t nudgeCounter = 0;
    while (userList.count(nextUID)){
@@ -96,7 +122,7 @@ uint_fast64_t PermissionsDB::add_user(std::string name, std::string username, ui
    }
    UserEntry temp;
    temp.name = name; temp.GID = GID; temp.username = username; temp.UID = nextUID;
-   if (scrypt_hashing(password, &(temp.salt), temp.hashedPass, HASH_OUTPUT_LENGTH)){
+   if (scrypt_generate_hash(password, temp)){
       userList.insert(std::pair<uint_fast64_t, UserEntry>(nextUID, temp));
       nextUID++;
       return nextUID - 1;
@@ -108,13 +134,21 @@ uint_fast64_t PermissionsDB::add_user(std::string name, std::string username, ui
 
 uint_fast64_t PermissionsDB::add_group(std::string name){
    size_t nudgeCounter = 0;
+   for (auto iter : groupList){
+      if (iter.second.name == name){
+         return iter.first; // Give back the same ID, this would be better as a bidirectional map?
+      }
+      if (iter.first == nextGID) nextGID++;
+   }
    while (groupList.count(nextGID)){
       if (nudgeCounter > 64){
          return 0; // Just give up.
       }
       nextGID++; nudgeCounter++;
    }
-   groupList.insert(std::pair<uint_fast64_t, GroupType>(nextGID, GroupType{name, nextGID++}));
+   GroupType temp(name, nextGID);
+   groupList.insert(std::pair<uint_fast64_t, GroupType>(nextGID, temp));
+   return nextGID++;
 }
 
 bool PermissionsDB::add_activity(std::string name, int permissions){
@@ -125,6 +159,7 @@ bool PermissionsDB::add_activity(std::string name, int permissions){
    temp.owner_rwe[2] = permissions & 4; temp.owner_rwe[1] = permissions & 2; temp.owner_rwe[0] = permissions & 1;
    temp.name = name;
    actionList.insert(std::pair<std::string, APRs>(name, temp));
+   return true;
 };
 
 bool PermissionsDB::add_target(std::string name, uint_fast64_t ownerID){
@@ -143,9 +178,12 @@ bool PermissionsDB::add_permission_to_activity(std::string name, uint_fast64_t G
    GroupPermissions temp(GID, perms);
    auto it = actionList.find(name);
    if (it != actionList.end()){
-      auto res = it->second.otherPermissions.insert(temp);
-      if (res.second == false){
-         return false;
+      auto existingEntry = it->second.otherPermissions.find(temp);
+      if (existingEntry != it->second.otherPermissions.end()){
+         (*existingEntry).setPerms(perms);
+      }
+      else{
+         it->second.otherPermissions.insert(temp);
       }
       return true;
    }
@@ -153,9 +191,107 @@ bool PermissionsDB::add_permission_to_activity(std::string name, uint_fast64_t G
 };
 
 
+bool PermissionsDB::toggle_after_hours(){ isAfterHours = !isAfterHours; return isAfterHours;};
+bool PermissionsDB::toggle_group_restricted_hours(uint_fast64_t GID){
+   if (afterHoursRestrictedGroups.count(GID)){
+      afterHoursRestrictedGroups.erase(GID);
+      return true;
+   }
+   afterHoursRestrictedGroups.insert(GID);
+   return true;
+}
+
+uint_fast64_t PermissionsDB::try_login(const std::string& username, const std::string& password)
+{
+   uint_fast64_t UID = get_UID_from_username(username);
+   if (UID == 0){
+      // If username information is also considered PHI, then maybe wait a bit
+      // and give a generic "Username or password incorrect" sort of thing.
+      std::cout << "Username not found." << std::endl;
+      return 0;
+   }
+
+   if (scrypt_hash_check(password, userList[UID])){
+      if (isAfterHours && afterHoursRestrictedGroups.count(userList[UID].GID)){
+         std::cout << "Sorry but you are not allowed access to the system outside business hours." << std::endl;
+         return 0;
+      }
+      return UID; // Using UID as token in this mess.
+   }
+   else{
+      std::cout << "Sorry but we could not verify your password with that username." << std::endl;
+      return 0;
+   }
+}
+
+int PermissionsDB::check_action_permissions(uint_fast64_t UID, const std::string& actionName, size_t target)
+{
+   uint_fast64_t usrGID;
+   try{
+      usrGID = userList.at(UID).GID;
+   }
+   catch (const std::out_of_range&){
+      // Also put some sort of error here, it probably shouldn't happen often in a real system.
+      // Unless someone is purposely trying to DDOS the system I guess.
+      return NONE_PERM;
+   }
+
+   if (isAfterHours && afterHoursRestrictedGroups.count(usrGID)){
+      std::cout << "Sorry but you are not allowed access to the system outside business hours." << std::endl;
+      return NONE_PERM;
+   }
+
+   try{
+      const APRs& activity = actionList.at(actionName);
+      FileData* temp = nullptr;
+      if (target < patientFiles.size()){
+         temp = patientFiles.data() + target; // C code fixes procrastination!
+      }
+      return activity.getPermissions(UID, usrGID, temp);
+   }
+   catch (const std::out_of_range&){
+      // Also put some sort of error here, it probably shouldn't happen often in a real system.
+      // Unless someone is purposely trying to DDOS the system I guess.
+      return NONE_PERM;
+   }
+   return NONE_PERM;
+}
+
+// How to debug, just use prints!
+void PermissionsDB::print_internal_variables()
+{
+   std::cout << "There are currently: " << userList.size() - 1 << " Users registered." << std::endl;
+   std::cout << "There are currently: " << groupList.size() - 1 << " Groups registered." << std::endl;
+   std::cout << "The next UIDs and GIDs to be distributed are: " << nextUID << " and " << nextGID << std::endl;
+   std::cout << "Here is the User list:" << std::endl;
+   for (auto iter : userList){
+      std::cout << iter.second.to_string() << "\n";
+   }
+   std::cout << "\nHere is the Group list:" << std::endl;
+   for (auto iter : groupList){
+      std::cout << iter.first << " " << iter.second.name << "\n";
+   }
+
+   std::cout << std::endl;
+   std::cout << "There are currently: " << patientFiles.size() << " Targets registered." << std::endl;
+   std::cout << "There are currently: " << actionList.size() << " Actions registered." << std::endl;
+   std::cout << "Here is the target list:" << std::endl;
+   for (auto iter : patientFiles){
+      std::cout << iter.name << " Owned by: " <<userList[iter.ownerID].name << "\n";
+   }
+   std::cout << "\nHere is the Action list with additional permissions:" << std::endl;
+   for (auto iter : actionList){
+      std::cout << "Name: " << iter.first << " Target owner permissions: " << bool_array_to_int(iter.second.owner_rwe) << " " << iter.second.otherPermissions.size() << " Group Permissions:" << "\n";
+      for (const GroupPermissions& permsIter : iter.second.otherPermissions){
+         std::cout << "\t" << groupList.at(permsIter.groupID).name << " " << bool_array_to_int(permsIter.rwe) << "\n";
+      }
+   }
+   std::cout << std::endl;
+}
+
 bool PermissionsDB::read_DB()
 {
-   std::fstream in; size_t i;
+   std::fstream in; size_t i = 0; std::string nextLine;
    size_t userListSize;
    size_t nextUID_file;
    size_t groupListSize;
@@ -176,11 +312,13 @@ bool PermissionsDB::read_DB()
       return false;
    }
 
-   for (i = 0; i < userListSize; i++){
-      std::string temp;
-      in >> temp;
-      UserEntry newUser(temp);
+   while(i < userListSize && std::getline(in, nextLine)){
+      if (nextLine.empty()){
+         continue;
+      }
+      UserEntry newUser(nextLine);
       userList.insert(std::pair<uint_fast64_t, UserEntry>(newUser.UID, newUser));
+      i++;
    }
 
    for (i = 0; i < patientFilesSize; i++){
@@ -199,7 +337,7 @@ bool PermissionsDB::read_DB()
       groupList.insert(std::pair<uint_fast64_t, GroupType>(GID, tempG));
    }
 
-   i = 0; std::string nextLine;
+   i = 0;
    while(i < actionListSize && std::getline(in, nextLine)){
       if (nextLine.empty()){
          continue;
@@ -242,16 +380,16 @@ bool PermissionsDB::write_DB()
       tempFile << iter.first << " " << iter.second.to_string() << "\n";
    }
    tempFile << std::endl; // Hopefully flush anything in the buffer?
-
+   tempFile.close(); // Don't try to move an open file, dummy
    try{
       std::filesystem::rename(tempFilePath, DBPath);
    }
-   catch ( std::filesystem::filesystem_error& ){
+   catch ( std::filesystem::filesystem_error& e ){
       std::cerr << "Failed to rename temporary file to database file, aborting.\n";
-      tempFile.close();
+      std::cerr << e.what() << std::endl;
       return false;
    }
-   tempFile.close();
+
    return true;
 }
 
--- SYSC4XXX/SYSC4810/A3_Code/main.cpp
+++ SYSC4XXX/SYSC4810/A3_Code/main.cpp
@@ -13,13 +13,10 @@ using std::endl;
 
 int main()
 {
-   // Testing PWChecker
-   PWChecker tester;
-
-   tester.print_values();
-
    std::filesystem::path newerPath("newDB.txt");
    PWDB::PermissionsDB testDB(newerPath);
+   std::cout << "Done initializing testDB" << std::endl;
+
    testDB.add_group("Radiologist");
    testDB.add_group("Physician");
    testDB.add_group("Nurse");
@@ -27,9 +24,52 @@ int main()
    testDB.add_group("Administrator");
    testDB.add_group("TechSupport");
 
+   testDB.add_user("Howard Linkler", "hlinkler", 1, "Trid142?");
+   testDB.add_user("Veronica Perez", "vperez", 1, "Trid142?");
+   testDB.add_user("Winston Callahan", "wcallahan", 2, "Trid142?");
+   testDB.add_user("Leslie Stewart", "lstewart", 2, "Trid142?");
+   testDB.add_user("Kelsey Chang", "kchang", 3, "Trid142?");
+   testDB.add_user("James Preston", "jpreston", 3, "Trid142?");
+   testDB.add_user("Rabul Garza", "rgarza", 4, "Trid142?");
+   testDB.add_user("Arjun Singh", "asingh", 4, "Trid142?");
+   testDB.add_user("Heather Anderson", "handerson", 5, "Trid142?");
+   testDB.add_user("Keikilana Kapahu", "kkapahu", 5, "Trid142?");
+   testDB.add_user("Harold Zakara", "hzakara", 6, "Trid142?");
+   testDB.add_user("Mailina Romanoca", "mromanoca", 6, "Trid142?");
+
+   testDB.add_target("RG_Profile", testDB.get_UID_from_username("rgarza"));
+   testDB.add_target("AS_Profile", testDB.get_UID_from_username("asingh"));
+   testDB.add_target("RG_History", testDB.get_UID_from_username("rgarza"));
+   testDB.add_target("AS_History", testDB.get_UID_from_username("asingh"));
+   testDB.add_target("RG_Images", testDB.get_UID_from_username("rgarza"));
+   testDB.add_target("AS_Images", testDB.get_UID_from_username("asingh"));
+   testDB.add_target("DIAG", 0);
+
+   // Patient gets their own profile, history, I'll assume physician is attached to profile.
+   testDB.add_activity("Patient_Profile", PWDB::READ); // Means that the patient gets read automatically
+   testDB.add_activity("Patient_History", PWDB::READ);
+   testDB.add_activity("Patient_Images");
+
+   testDB.add_activity("Access_System"); // Dummy activity to restrict Administrators from doing it...
+
+   testDB.add_activity("Diagnose_Patient");
+   testDB.add_activity("Prescribe");
+   testDB.add_activity("Diagnose_Equipment");
+
+   // Everyone except IT.
+   for (size_t i = 1; i < 6; i++){
+      testDB.add_permission_to_activity("Patient_Profile", i, PWDB::READ);
+   }
+   testDB.add_permission_to_activity("Patient_Profile", 5, PWDB::WRITE); // Admins can write.
+   testDB.toggle_group_restricted_hours(5); // Admins can't work after hours, lucky
 
 
-   cout << "Hello world!" << endl;
+
+
+   testDB.print_internal_variables();
+   //testDB.read_DB();
+   //testDB.print_internal_variables();
+
    return 0;
 }
 
